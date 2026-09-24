6a714b0961b1a5a359414acba27860bf21804775
dxtrb <theamazingdexterb@gmail.com>
2026-09-24 13:44:05 -0400

    Add files via upload

---
+++ projects/js/project04/project01-04.js
@@ -0,0 +1,22 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Dexter Boynton
+    Date:   9/24/2026
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+//home
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+//work
+let workStreet = "15 Oak Ln.";
+let workCity = "Central City";
+let workState = "MA";
+let workCode = "02104";
+
